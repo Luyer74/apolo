65919085900d66dd83cc69f3f79013dc8db07e00
Luis Yerik Arambula <A00825080@itesm.mx>
2022-11-02 17:55:41 -0600

    finish hero

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -4,6 +4,7 @@ import {
   Button,
   Heading,
   VStack,
+  Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
 import React from 'react';
@@ -11,8 +12,7 @@ import React from 'react';
 export default function Hero() {
   return (
     <Flex
-      h="500px"
-      mt={2}
+      h="600px"
       backgroundImage={
         'url(https://images.unsplash.com/photo-1544207916-df3b3a131e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)'
       }
@@ -22,36 +22,32 @@ export default function Hero() {
       <VStack
         w={'full'}
         justify={'center'}
-        px={useBreakpointValue({ base: 4, md: 8 })}
+        align="left"
         bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
       >
-        <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+        <Stack
+          maxW={'2xl'}
+          align={'flex-start'}
+          spacing={6}
+          position="relative"
+          left="100px"
+        >
           <Heading
             color={'white'}
             as="h1"
             fontWeight="extrabold"
             lineHeight={1.2}
             maxW="570px"
-            fontSize={useBreakpointValue({ base: '100px', md: '56px' })}
+            fontSize={useBreakpointValue({ base: '40px', md: '56px' })}
           >
             Manten a tus clientes conectados. Donde sea.
           </Heading>
+          <Text color="white" fontSize="28px" maxW="600px">
+            Nuestras redes manejan las cargas más pesadas a la mejor velocidad.
+          </Text>
           <Stack direction={'row'}>
-            <Button
-              bg={'blue.400'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'blue.500' }}
-            >
-              Show me more
-            </Button>
-            <Button
-              bg={'whiteAlpha.300'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'whiteAlpha.500' }}
-            >
-              Show me more
+            <Button bg="color3" rounded={'full'} color={'white'}>
+              Paquetes Disponibles
             </Button>
           </Stack>
         </Stack>
